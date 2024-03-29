f5c744cdd193651c83738c025773049d011869c6
Rares Matei <rares.matei@doctarigroup.com>
2024-03-29 14:35:21 +0200

    removed cloud

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  # cloud {
+  #   organization = "rares-dev"
+  #   workspaces {
+  #     name = "learn-tfc-aws"
+  #   }
+  # }
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami = "ami-0a23a9827c6dab833"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
